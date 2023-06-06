7a284c1a180f4dc35b2ea65d1538b9cf72492333
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2023-06-05 23:05:02 -0300

    chore: add new setings

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,12 @@
+import { pathsToModuleNameMapper, JestConfigWithTsJest } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
 export default {
   clearMocks: true,
   coverageProvider: "v8",
   preset: "ts-jest",
-};
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    useESM: true,
+  }),
+
+} as JestConfigWithTsJest
